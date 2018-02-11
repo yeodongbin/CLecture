2d7e85c56dd5d8a24e3d35048519fcb1ae4b53d1
yeodongbin <yeodongbin2011@gmail.com>
2018-02-12 00:37:48 +0900

    2018-02-12 update

--- 4.1.Operator-.c
+++ 4.1.Operator-.c
@@ -10,83 +10,83 @@
 //14 대입연산자 = += -= *= /= %= &= ^= |= <<= >>=
 //15 콤마연산자 , 
 //
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int a, b;
-//	double da, db;
-//	int sum, sub, mul, inv, div, remainder;
-//
-//	a = 30;
-//	b = 20;
-//	da = 2.0;
-//	db = 0.3;
-//
-//	//산술연산자
-//	sum = a + b;
-//	sub = a - b;
-//	mul = a * b;
-//	inv = -a;
-//	div = a / b;
-//	remainder = a % b;
-//
-//	printf("%d\t", sum);
-//	printf("%d\t", sub);
-//	printf("%d\t", mul);
-//	printf("%d\t", inv);
-//	printf("%d\t", div);
-//	printf("%d\t", remainder);
-//
-//	printf("\n%.2f\n", da / db);
-//	//printf("\n%.2f\t", da % db); 불가
-//
-//	//***********************************
-//	//증감 연산자 a++, a--
-//	int pre, post;
-//
-//	a = 10;
-//	b = 10;
-//
-//	pre = (++a) * 3; //시작전
-//	post = (b++) * 3; //끝난후
-//
-//	printf("pre  = %d, a = %d\n", pre, a);
-//	printf("posd = %d, b = %d\n", post, b);
-//
-//	//***********************************
-//	//비트 연산자
-//  
-//
-//
-//	//***********************************
-//	//관계 연산자 > < >= <=  == != ture(1), falue(2)s
-//	a = 20;
-//	b = 10;
-//
-//	printf("a> b => %d\n", a>b);
-//	printf("a>=b => %d\n", a>=b);
-//	printf("a< b => %d\n", a<b);
-//	printf("a<=b => %d\n", a<=b);
-//	printf("a==b => %d\n", a==b);
-//	printf("a!=b => %d\n", a!=b);
-//
-//	//***********************************
-//	//논리 연산자 //&& || ! 3가지
-//	int a = 30;
-//
-//	printf("%d\n", (a > 0) && (a > 20));
-//	// 1 && 1
-//	printf("%d\n", (a < 0) && (a < 20));
-//	// 0 && 0
-//
-//	printf("%d\n", (a > 0) || (a > 20));
-//	// 1 && 1
-//	printf("%d\n", (a > 0) || (a < 20));
-//	// 1 && 0
-//
-//	printf("%d\n", !(a > 0));
-//	1
-//
-//	return 0;
-//}
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	double da, db;
+	int sum, sub, mul, inv, div, remainder;
+
+	a = 30;
+	b = 20;
+	da = 2.0;
+	db = 0.3;
+
+	//산술연산자
+	sum = a + b;
+	sub = a - b;
+	mul = a * b;
+	inv = -a;
+	div = a / b;
+	remainder = a % b;
+
+	printf("%d\t", sum);
+	printf("%d\t", sub);
+	printf("%d\t", mul);
+	printf("%d\t", inv);
+	printf("%d\t", div);
+	printf("%d\t", remainder);
+
+	printf("\n%.2f\n", da / db);
+	//printf("\n%.2f\t", da % db); 불가
+
+	//***********************************
+	//증감 연산자 a++, a--
+	int pre, post;
+
+	a = 10;
+	b = 10;
+
+	pre = (++a) * 3; //시작전
+	post = (b++) * 3; //끝난후
+
+	printf("pre  = %d, a = %d\n", pre, a);
+	printf("posd = %d, b = %d\n", post, b);
+
+	//***********************************
+	//비트 연산자
+  
+
+
+	//***********************************
+	//관계 연산자 > < >= <=  == != ture(1), falue(2)s
+	a = 20;
+	b = 10;
+
+	printf("a> b => %d\n", a>b);
+	printf("a>=b => %d\n", a>=b);
+	printf("a< b => %d\n", a<b);
+	printf("a<=b => %d\n", a<=b);
+	printf("a==b => %d\n", a==b);
+	printf("a!=b => %d\n", a!=b);
+
+	//***********************************
+	//논리 연산자 //&& || ! 3가지
+	int a = 30;
+
+	printf("%d\n", (a > 0) && (a > 20));
+	// 1 && 1
+	printf("%d\n", (a < 0) && (a < 20));
+	// 0 && 0
+
+	printf("%d\n", (a > 0) || (a > 20));
+	// 1 && 1
+	printf("%d\n", (a > 0) || (a < 20));
+	// 1 && 0
+
+	printf("%d\n", !(a > 0));
+
+
+	return 0;
+}
--- 4.2.BitOperator_Etc.c
+++ 4.2.BitOperator_Etc.c
@@ -1,72 +1,80 @@
-////기타 연산자
-////형변환    (int)10.7 -> 10
-////sizeof   sizeof(double) -> 8 byte
-////복합대입   a += 10;->a = a + 10; + - * / %
-////콤마연산자  res = (a, b)->res = b;
-////조건연산자(삼항연산자) res = (a > b) ? a : b;
-////비트연산자  & | ^ ~ >> <<
-//
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	//형변환
-//	int a = 20;
-//	int b = 3;
-//	int c = 2;
-//	int d = 1;
-//	int res;
-//	float fRes;
-//
-//	printf("%.2f, %.2f\n", ((float)a), ((float)b));
-//	printf("%.2f\n", ((float)a) / ((float)b));//6.67
-//	printf("%d\n", (int)(((float)a) / ((float)b)));//6.67
-//
-//	//sizeof
-//	printf("int = %d byte\n", sizeof(int));
-//	printf("double = %d byte\n", sizeof(double));
-//	printf("float = %d byte\n", sizeof(float));
-//	printf("char = %d byte\n", sizeof(char));
-//	printf("int value = %d byte\n", sizeof(a));
-//	printf("int value = %d byte\n", sizeof(10));
-//
-//	//복합대입 + - * / % 연산값을 피연산자에 다시 저장한다.
-//	a += 10; //a = a +10; a =30
-//	printf("%d\n", a);
-//	b -= 1;  //b = b - 1; b =2
-//	printf("%d\n", b);
-//	c *= 2;  //c = c * 2; c =4
-//	printf("%d\n", c);
-//	c /= 2;  //c = 2
-//	printf("%d\n", c);
-//	c %= 2;  //c = 0;
-//	printf("%d\n", c);
-//
-//	//콤마 연산자
-//	a = 10;
-//	b = 20;
-//
-//	printf("%d\n", (a += 10, b *= 20)); // b
-//
-//	//조건 연산자 (조건문)? 참 : 거짓; //매크로 속도 빨라
-//	a = 10;
-//	b = 20;
-//
-//	res = (a < b) ? 1000 : 10; //10
-//	printf("%d\n", res); // b
-//
-//	//비트 연산자, & | ^ ~ >> << 정수형에서만 사용가능 -> 복합대입 연산자 사용가능
-//	a = 1;  // 0001
-//	b = 2;  // 0010
-//
-//	printf("%d\n", a&b); // b
-//	printf("%d\n", a|b); // b
-//	printf("%d\n", a^b); // b
-//	printf("%d\n", ~a); // b
-//	printf("%d\n", a>>1); // b
-//	printf("%d\n", a<<1); // b
-//
-//	//산술연산자( + - ) > 관계연산자 ( &&, || ) > 논리 연산자 (& | ^ )
-//
-//	return 0;
-//}
+//기타 연산자
+//형변환    (int)10.7 -> 10
+//sizeof   sizeof(double) -> 8 byte
+//복합대입   a += 10;->a = a + 10; + - * / %
+//콤마연산자  res = (a, b)->res = b;
+//조건연산자(삼항연산자) res = (a > b) ? a : b;
+//비트연산자  & | ^ ~ >> <<
+
+#include <stdio.h>
+
+int main(void)
+{
+	//형변환
+	int a = 20;
+	int b = 3;
+	int c = 2;
+	int d = 1;
+	int res;
+	double dRes;
+
+	printf("%.2f, %.2f\n", ((double)a), ((double)b));
+	printf("%.2f\n", ((double)a) / ((double)b));//6.67
+	printf("%d\n", (int)(((double)a) / ((double)b)));//6.67
+	
+	dRes = ((double)a) / ((double)b);
+	res = (int)dRes;
+
+	printf("dRes = %lf\n",dRes);
+	printf("res = %d\n",res);
+
+	//sizeof 피연산자의 크기를 바이트단위로 계산한다.
+	printf("int = %d byte\n", sizeof(int));
+	printf("double = %d byte\n", sizeof(double));
+	printf("float = %d byte\n", sizeof(float));
+	printf("char = %d byte\n", sizeof(char));
+	printf("int value = %d byte\n", sizeof(a));
+	printf("int value = %d byte\n", sizeof(10));
+
+	//복합대입 + - * / % 연산값을 피연산자에 다시 저장한다.
+	a += 10; //a = a +10; a =30
+	printf("%d\n", a);
+	b -= 1;  //b = b - 1; b =2
+	printf("%d\n", b);
+	c *= 2;  //c = c * 2; c =4
+	printf("%d\n", c);
+	c /= 2;  //c = 2
+	printf("%d\n", c);
+	c %= 2;  //c = 0;
+	printf("%d\n", c);
+
+	//콤마 연산자
+	a = 10;
+	b = 20;
+
+	printf("%d\n", (a += 10, b *= 20)); // b
+
+	//조건 연산자 (조건문)? 참 : 거짓; //매크로 속도 빨라
+	a = 10;
+	b = 20;
+
+	res = (a < b) ? 1000 : 10; //10
+	printf("%d\n", res); // b
+
+	//비트 연산자, & | ^ ~ >> << 정수형에서만 사용가능 -> 복합대입 연산자 사용가능
+	a = 1;  // 0001
+	b = 2;  // 0010
+
+	printf("%d\n", a&b); // b
+	printf("%d\n", a|b); // b
+	printf("%d\n", a^b); // b
+	printf("%d\n", ~a); // b
+	printf("%d\n", a>>1); // b
+	printf("%d\n", a<<1); // b
+
+	//산술연산자( + - ) > 관계연산자 ( &&, || ) > 논리 연산자 (& | ^ )
+
+	// 주소연산자(&) 간접참조 연산자(*) 포인터 
+	// 멤머접근 연산자(.)  간접멤버접근연산자(->) 구제체
+	return 0;
+}
