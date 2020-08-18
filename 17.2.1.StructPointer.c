46959466dde8f1eb923eb64a94a9e991ed93bc82
Yeo, Dongbin <yeodongbin2011@gmail.com>
2020-08-19 02:33:01 +0900

    divided compile

--- 17.2.1.StructPointer.c
+++ 17.2.1.StructPointer.c
@@ -1,54 +1,54 @@
-////구조체의 포인터 설정은 동일
-////구조체 포인터를 활용하여 값을 가져오는 것은 ->
-//
-//#include <stdio.h>
-//
-//typedef struct
-//{
-//	int kor;
-//	int eng;
-//	int mat;
-//} _score;
-//
-//void print_list(_score* list, int count);
-//
-//void main(void)
-//{
-//	//1구조체 포인터
-//	_score dongbin = { 90,80,70 };
-//	_score *pstu = &dongbin;
-//	printf("kor : %d\n", (*pstu).kor); //dongbin.kor
-//	printf("eng : %d\n", pstu->eng);
-//	printf("mat : %d\n", pstu->mat);
-//
-//
-//	//2 구조체 배열의 초기화 방법은 배열과 동일 
-//	_score list[3] = {
-//		{100,100,100},
-//		{60,60,60},
-//		{40,40,40}};
-//	int i = 0;
-//	int count = sizeof(list) / sizeof(_score); //sizeof(int)
-//	for (i = 0; i < count; i++)
-//	{
-//		printf("국어: %3d, 영어: %3d, 수학: %3d\n",
-//			list[i].kor, list[i].eng, list[i].mat);
-//	}
-//
-//	//3 구조체 포인터 매개변수
-//	print_list(list, count); //배열은 주소값
-//
-//	return;
-//}
-//
-//void print_list(_score* list, int count)
-//{
-//	int i = 0;
-//	
-//	printf("%d\n", count);
-//	for (i = 0; i < count; i++)
-//	{
-//		printf("국어: %3d, 영어: %3d, 수학: %3d\n",
-//			list[i].kor, list[i].eng, list[i].mat);
-//	}
-//}
+//구조체의 포인터 설정은 동일
+//구조체 포인터를 활용하여 값을 가져오는 것은 ->
+
+#include <stdio.h>
+
+typedef struct
+{
+	int kor;
+	int eng;
+	int mat;
+} _score;
+
+void print_list(_score* list, int count);
+
+void main(void)
+{
+	//1구조체 포인터
+	_score dongbin = { 90,80,70 };
+	_score *pstu = &dongbin;
+	printf("kor : %d\n", (*pstu).kor); //dongbin.kor
+	printf("eng : %d\n", pstu->eng);
+	printf("mat : %d\n", pstu->mat);
+
+
+	//2 구조체 배열의 초기화 방법은 배열과 동일 
+	_score list[3] = {
+		{100,100,100},
+		{60,60,60},
+		{40,40,40}};
+	int i = 0;
+	int count = sizeof(list) / sizeof(_score); //sizeof(int)
+	for (i = 0; i < count; i++)
+	{
+		printf("국어: %3d, 영어: %3d, 수학: %3d\n",
+			list[i].kor, list[i].eng, list[i].mat);
+	}
+
+	//3 구조체 포인터 매개변수
+	print_list(list, count); //배열은 주소값
+
+	return;
+}
+
+void print_list(_score* list, int count)
+{
+	int i = 0;
+	
+	printf("%d\n", count);
+	for (i = 0; i < count; i++)
+	{
+		printf("국어: %3d, 영어: %3d, 수학: %3d\n",
+			list[i].kor, list[i].eng, list[i].mat);
+	}
+}
--- 17.2.7.typedef.c
+++ 17.2.7.typedef.c
@@ -1,54 +1,54 @@
-//
-//#include <stdio.h>
-//
-////공용체, 자기참조 구조체 제외
-////포인터 공부이후에 자기참조, 구조체 포인터 매개변수
-//
-////typedef, 
-////typedef 변수를 재정의 해서 보기 편한 변수명으로 변경
-////struct student
-////{
-////	int num;
-////	double grade;
-////};
-////
-////typedef struct student Student; //struct student => Student
-////
-//////2
-////typedef struct
-////{
-////	int num;
-////	double grade;
-////} Student;
-//
-////3
-//typedef struct student
-//{
-//	int num;
-//	double grade;
-//} Student;
-//
-//void print_data(Student ps);
-//
-//int main(void)
-//{
-//	Student s1 = { 315, 4.2 };
-//	print_data(s1);
-//
-//	typedef int BYTE;
-//
-//	BYTE a;
-//
-//	a = 10;
-//
-//
-//	return 0;
-//}
-//
-//void print_data(Student ps)
-//{
-//	printf("학번 : %d\n", ps.num);
-//	printf("학점 : %.2f\n", ps.grade);
-//}
 
+#include <stdio.h>
+
+//공용체, 자기참조 구조체 제외
+//포인터 공부이후에 자기참조, 구조체 포인터 매개변수
+
+//typedef, 
+//typedef 변수를 재정의 해서 보기 편한 변수명으로 변경
+struct student
+{
+	int num;
+	double grade;
+};
+
+typedef struct student Student; //struct student => Student
+
+//2
+typedef struct
+{
+	int num;
+	double grade;
+} Student;
+
+//3
+typedef struct student
+{
+	int num;
+	double grade;
+} Student;
+
+void print_data(Student ps);
+
+int main(void)
+{
+	Student s1 = { 315, 4.2 };
+	print_data(s1);
+
+	typedef int BYTE;
+
+	BYTE a;
+
+	a = 10;
+
+
+	return 0;
+}
+
+void print_data(Student ps)
+{
+	printf("학번 : %d\n", ps.num);
+	printf("학점 : %.2f\n", ps.grade);
+}
+//
 //563 - 5
